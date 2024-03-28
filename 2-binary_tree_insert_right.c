ad3a8d8c2bbadbb50cc32db33817e974627d1930
yaredybk <yb12ybk@gmail.com>
2024-03-28 13:27:50 -0700

    fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right;
 
+	if (parent == NULL)
+		return (NULL);
+
 	right =  binary_tree_node(parent, value);
 	if (right == NULL)
 		return (NULL);
