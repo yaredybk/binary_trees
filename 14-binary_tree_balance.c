c1ea6ad889c9707949c43bdd2b4a6cc17cb99b5e
yaredybk <yb12ybk@gmail.com>
2024-03-29 01:00:08 -0700

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,6 +12,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	h = binary_tree_height(tree->left) - binary_tree_height(tree->right)
+	h = binary_tree_height(tree->left) - binary_tree_height(tree->right);
 	return (h);
 }
